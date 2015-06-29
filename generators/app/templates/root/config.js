705bea47f83cd37a9726792aba8d347221c463b7
telekosmos <telekosmos@ymail.com>
2015-06-29 19:22:49 +0200

    ES6 main generator working

---
+++ generators/app/templates/root/config.js
@@ -0,0 +1,16 @@
+System.config({
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime",
+      "es7.decorators",
+      "es7.classProperties"
+    ]
+  },
+  "paths": {
+    "*": "dist/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js",
+    "aurelia-skeleton-navigation/*": "dist/*.js"
+  }
+});
