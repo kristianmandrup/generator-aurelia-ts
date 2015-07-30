12bab1138e0648b1dfc37d2df4b0aa651cade861
Kristian Mandrup <kmandrup@gmail.com>
2015-07-30 15:32:08 +0100

    stylus task fix

--- generators/styles/templates/styles/tasks/_stylus.js
+++ generators/styles/templates/styles/tasks/_stylus.js
@@ -2,23 +2,15 @@ var gulp = require('gulp');
 var paths = require('../paths');
 var stylus = require('gulp-stylus');
 var sourcemaps = require('gulp-sourcemaps');
-<% if (axis) { %>
-var axis = require('axis');
-<% } %>
-<% if (rupture) { %>
-var rupture = require('rupture');
-<% } %>
-<% if (jeet) { %>
-var jeet = require('jeet');
-<% } %>
-<% if (autoprefixer) { %>
-var autoprefixer = require('autoprefixer');
-<% } %>
+<% if (axis) { %>var axis = require('axis');<% } %>
+<% if (rupture) { %>var rupture = require('rupture');<% } %>
+<% if (jeet) { %>var jeet = require('jeet');<% } %>
+<% if (autoprefixer) { %>var autoprefixer = require('autoprefixer');<% } %>
 
 gulp.task('stylus', function () {
   gulp.src(paths.stylus)
     .pipe(sourcemaps.init())
-    .pipe(stylus({'include css': true}).use([<% useList %>])
+    .pipe(stylus({'include css': true}).use([<% useList %>]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styleDest));
 });
