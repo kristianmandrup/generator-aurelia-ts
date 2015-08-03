d9f2b759f947c6afbaaf42be0c433c04e317305c
Kristian Mandrup <kmandrup@gmail.com>
2015-08-03 12:59:23 +0100

    good plugins gen

--- generators/layout/index.js
+++ generators/layout/index.js
@@ -5,26 +5,19 @@ var yosay = require('yosay');
 require('sugar');
 var fs = require('fs');
 var generator;
-
 let lib = require('../../lib');
 let write = require('./write');
 let prompts = require('./prompts');
 let util = require('./util');
 let install = require('./install');
-let writer = lib.writer;
-let copy = lib.copy;
-let options = lib.options;
 let info = lib.log.info;
 
 module.exports = yeoman.generators.Base.extend({
-
   // note: arguments and options should be defined in the constructor.
   constructor: function() {
     yeoman.generators.Base.apply(this, arguments);
-
     generator = this;
     this.props = {};
-
     this.option('fa'); // font awesome
     // ui framework options
     this.option('ui', {type: 'string'});
@@ -33,13 +26,13 @@ module.exports = yeoman.generators.Base.extend({
   initializing: function() {
     this.appTitle = this.options.appTitle || 'My App';
     this.appDesc = this.options.appDesc || 'No description...';
-    this.props.uiFramework = options.mapUi(this.options.ui);
+    this.props.uiFramework = lib.options.mapUi(this.options.ui);
     this.props.fa = this.options.fa;
 
     this.myPrompts = prompts.createFor(this.props);
     this.myUtil = require('./util')(this);
-    this.writer = writer(write(this));
-    this.copy = copy(this);
+    this.writer = lib.writer(write(this));
+    this.copy = lib.copy(this);
     this.install = lib.install(this);
     this.util = lib.util;
   },
@@ -51,9 +44,8 @@ module.exports = yeoman.generators.Base.extend({
       this.prompt(this.myPrompts.phase1(), function(answers) {
         // TODO: extract util
         this.cssFrameworks = this.util.isEmpty(answers.cssFrameworks) ? ['None'] : answers.cssFrameworks;
-
         this.fontAwesome = answers.fontAwesome || this.props.fa;
-        this.ui = options.uiFramework(this.cssFrameworks);
+        this.ui = lib.options.uiFramework(this.cssFrameworks);
         // this.config.save();
         done();
       }.bind(this));
@@ -61,7 +53,6 @@ module.exports = yeoman.generators.Base.extend({
 
     phase2: function() {
       var done = this.async();
-
       this.prompt(this.myPrompts.phase2(this), function(answers) {
         this.primary = answers.primary;
         if (this.primary == 'None') this.cssFrameworks = [];
